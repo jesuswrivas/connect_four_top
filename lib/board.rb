46c99a226510f592f1673a8a55a8524aa7b42057
Jesus <jesuswrivas@gmail.com>
2023-03-28 16:43:23 -0500

    Create Board.make_move method
    Create make move method on Board class. The instance variable @board
    is an array of 7 arrays, each one represents a row of the game board.
    The method pushes the player's piece to the row selected
    
    Create #make_move test suite on board_spec.rb.
    Both tests passed

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,13 @@
 class Board
+    def initialize(board = [[],[],[],[],[],[],[],[]], players = [])
+        @board = board
+        @players = players
+    end
+
+    def make_move(row, player)
+        @board[row] << player
+    end
 
 end
+
+
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,5 +3,26 @@ require_relative "../lib/board.rb"
 
 describe Board do
 
+subject(:board){Board.new}
+let(:player_1){instance_double("Player", name: "jesus", mark: "green")}
 
+    describe "#make_move" do
+        context "When making a first move on row 0" do
+            #The default board, is empty
+            it "Places the piece on the first slot of row 0" do
+                board.make_move(0,player_1)
+                expect(board.instance_variable_get("@board")[0].length).to eq(1)
+            end    
+        end
+        context "When making a second move on row 0" do    
+            before do
+                #Row 0 now has one piece,
+                board.make_move(0, player_1)
+              end
+            it "Places the second piece on the second slot of row 0" do
+                board.make_move(0,player_1)
+                expect(board.instance_variable_get("@board")[0].length).to eq(2)
+            end
+        end
+    end
 end
