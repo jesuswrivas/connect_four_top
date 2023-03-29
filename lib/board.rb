dffb55eee511989ffd1da2d0bd17e3277f813105
Jesus <jesuswrivas@gmail.com>
2023-03-29 09:29:06 -0500

    Create valid_move? on Board class
    The valid_move? checks if the integer input matches the boards dimentions.
    
    Create valid_move? tests on board_spec.rb
    
    All tests passed.

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,18 @@
 class Board
-    def initialize(board = [[],[],[],[],[],[],[],[]], players = [])
+    def initialize(board = [[],[],[],[],[],[],[]], players = [])
         @board = board
         @players = players
     end
 
-    def make_move(row, player)
-        @board[row] << player
+    def make_move(integer, player)
+    #Integer needs to be between 1 and 7 (there are 7 columns). We will error check on the Display.ask_input function
+        @board[integer-1] << player
+    end
+
+    def valid_move?(integer)
+        return false if !(integer - 1).between?(0,6)
+        return false if @board[integer - 1].length == 6
+        true
     end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,22 +7,55 @@ subject(:board){Board.new}
 let(:player_1){instance_double("Player", name: "jesus", mark: "green")}
 
     describe "#make_move" do
-        context "When making a first move on row 0" do
+        context "When making a first move on column 1" do
             #The default board, is empty
-            it "Places the piece on the first slot of row 0" do
-                board.make_move(0,player_1)
+            it "Places the piece on the first slot of column 0" do
+                board.make_move(1,player_1)
+            #When accesing to the instance variable, we need to be careful with the indexes. In this case even thought we are referring to "column 1"
+            #Remember that in the instance variable @board, column 1 has an index of 0 and column 7 (the last one) has an index of 6
                 expect(board.instance_variable_get("@board")[0].length).to eq(1)
             end    
         end
-        context "When making a second move on row 0" do    
+        context "When making a second move on column 0" do    
             before do
-                #Row 0 now has one piece,
-                board.make_move(0, player_1)
-              end
-            it "Places the second piece on the second slot of row 0" do
-                board.make_move(0,player_1)
+                #column 0 now has one piece,
+                board.make_move(1, player_1)
+            end
+            it "Places the second piece on the second slot of column 0" do
+                board.make_move(1,player_1)
                 expect(board.instance_variable_get("@board")[0].length).to eq(2)
             end
         end
     end
+
+    describe "#valid_move?" do
+    subject(:board){Board.new([[],[player_1, player_1,player_1, player_1],[],[],[player_1,player_1],[],[player_1, player_1,player_1, player_1,player_1, player_1]])}
+        
+        #In the board consists of 7 columns, each column has a slot for a chip (up to 6 slots)
+        context "When the column selected is column 5 (2 chips)" do
+            it "returns true" do
+                expect(board.valid_move?(5)).to eq(true)
+            end
+        end
+        context "When the column selected is column 2 (4 chips)" do
+            it "returns true" do
+                expect(board.valid_move?(2)).to eq(true)
+            end
+        end
+        context "When the column selected is column 7 (6 chips)" do
+            it "returns false" do
+                expect(board.valid_move?(7)).to eq(false)
+            end
+            
+        end
+        context "When the column selected is column 10" do
+            #We perform error check on another function, which makes sure that the integer provided is between 1 and 7
+            #However we test it anyways,
+            it "returns false" do
+                expect(board.valid_move?(10)).to eq(false)
+            end    
+        end
+    end
+
+
 end
